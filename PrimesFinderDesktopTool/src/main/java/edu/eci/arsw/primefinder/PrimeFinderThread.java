5ef5c5a5052b84430542922d747930e88186d275
NicolasAguilera9906 <nicolasaguileracontreras@gmail.com>
2020-09-10 13:36:59 -0500

    Parte 1 y 2

--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,25 +1,52 @@
 package edu.eci.arsw.primefinder;
 
+import edu.eci.arsw.math.MathUtilities;
+
 import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrimeFinderThread extends Thread {
     private BigInteger start;
     private BigInteger end;
-    private PrimeFinder primeFinder;
-    private PrimesResultSet primesResultSet;
-    public PrimeFinderThread(BigInteger start, BigInteger end, PrimeFinder primeFinder, PrimesResultSet primesResultSet) {
+    private static PrimesResultSet primesResultSet;
+    private static boolean pause;
+    private static AtomicInteger amountOfNumbersProcessed;
+    private static Object object = new Object();
+
+    public PrimeFinderThread(BigInteger start, BigInteger end, PrimesResultSet primesResultSet , AtomicInteger amountOfNumbersProcessed) {
         this.start = start;
         this.end = end;
-        this.primeFinder=primeFinder;
         this.primesResultSet=primesResultSet;
+        this.amountOfNumbersProcessed = amountOfNumbersProcessed;
     }
+
 
     @Override
     public void run() {
-        for (BigInteger i = start;
-             i.compareTo(end) < 0;
-             i = i.add(BigInteger.ONE)) {
-            System.out.println(i);
+        for (BigInteger i = start; i.compareTo(end) < 0; i = i.add(BigInteger.ONE)) {
+            synchronized (object) {
+                if(pause) {
+                    try {
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            MathUtilities mt = new MathUtilities();
+            if (mt.isPrime(i)){
+                primesResultSet.addPrime(i);
+            }
+            amountOfNumbersProcessed.incrementAndGet();
         }
     }
+
+    public synchronized void pause(){
+        this.pause = true;
+    }
+    public synchronized void restart(){
+        this.pause = false;
+        notifyAll();
+    }
+
 }
--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
@@ -3,6 +3,8 @@ package edu.eci.arsw.primefinder;
 import edu.eci.arsw.mouseutils.MouseMovementMonitor;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.http.HttpResponse;
@@ -15,58 +17,59 @@ import org.apache.http.impl.client.HttpClientBuilder;
 public class PrimesFinderTool {
 
     private static final BigInteger LOWEST_NUMBER=new BigInteger(String.valueOf(1));
-    private static final BigInteger MAXIMUM_NUMBER=new BigInteger(String.valueOf(10));
+    private static final BigInteger MAXIMUM_NUMBER=new BigInteger(String.valueOf(10000));
     private static final int NUM_OF_THREADS=4;
     private static final BigInteger AMOUNT_OF_NUMBERS=MAXIMUM_NUMBER.subtract(LOWEST_NUMBER).add(new BigInteger(String.valueOf(1)));
-    private static PrimeFinder primeFinder;
+    private static AtomicInteger amountOfNumbersProcessed = new AtomicInteger();
+    private static PrimesResultSet primesResultSet;
 
 	public static void main(String[] args) {
-	        primeFinder = new PrimeFinder();
-            PrimesResultSet prs=new PrimesResultSet("Nicolas");
+	        amountOfNumbersProcessed.set(0);
+            primesResultSet=new PrimesResultSet("Nicolas");
 	        PrimeFinderThread[] threads = new PrimeFinderThread[NUM_OF_THREADS];
-            BigInteger start=new BigInteger(String.valueOf(1));
+            BigInteger start=new BigInteger(String.valueOf(0));
             BigInteger amount = AMOUNT_OF_NUMBERS.divide(new BigInteger(String.valueOf(NUM_OF_THREADS)));
             for(int i=0;i<NUM_OF_THREADS;i++) {
                 if (i == NUM_OF_THREADS - 1) {
                     BigInteger extra = AMOUNT_OF_NUMBERS.mod(new BigInteger(String.valueOf(NUM_OF_THREADS)));
                     BigInteger end = amount.add(extra).add(start);
-                    threads[i] = new PrimeFinderThread(start, end, primeFinder , prs);
+                    threads[i] = new PrimeFinderThread(start, end, primesResultSet,amountOfNumbersProcessed);
                 } else {
                     BigInteger end = amount.add(start);
-                    threads[i] = new PrimeFinderThread(start, end, primeFinder , prs);
+                    threads[i] = new PrimeFinderThread(start, end, primesResultSet,amountOfNumbersProcessed);
                 }
-                threads[i].start();
                 start = start.add(amount);
+                threads[i].start();
             }
-            int maxPrim=1000;
 
-
-            PrimeFinder.findPrimes(new BigInteger("1"), new BigInteger("10000"), prs);
             
             System.out.println("Prime numbers found:");
             
-            System.out.println(prs.getPrimes());
+            System.out.println(primesResultSet.getPrimes());
 
-            /*while(task_not_finished){
+            while(AMOUNT_OF_NUMBERS.compareTo(BigInteger.valueOf(amountOfNumbersProcessed.get())) > 0){
                 try {
-                    //check every 10ms if the idle status (10 seconds without mouse
-                    //activity) was reached. 
                     Thread.sleep(10);
-                    if (MouseMovementMonitor.getInstance().getTimeSinceLastMouseMovement()>10000){
-                        System.out.println("Idle CPU ");
-                    }
-                    else{
-                        System.out.println("User working again!");
-                    }
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(PrimesFinderTool.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }*/
-                        
-            
-            
-            
-            
+                if (MouseMovementMonitor.getInstance().getTimeSinceLastMouseMovement()>10000){
+                    for(int i=0;i<NUM_OF_THREADS;i++) {
+                        threads[i].pause();
+                    }
+                    System.out.println("PAUSA!");
+                    System.out.println("Primos encontrados hasta el momento");
+                    System.out.println(primesResultSet.getPrimes());
+                }
+                else{
+                    System.out.println("Trabajando");
+                    for(int i=0;i<NUM_OF_THREADS;i++) {
+                        threads[i].restart();
+                    }
+                }
+            }
+        System.out.println("Primos encontrados");
+        System.out.println(primesResultSet.getPrimes());
 	}
 	
 }
